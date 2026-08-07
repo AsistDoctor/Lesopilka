68380ea191f3f3c9630c4007551572418c9296fc
AsistDoctor <rebyssine@gmail.com>
2026-08-07 21:15:58 +0300

    Добавлены CSV-ридер, врайтер

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import models.Log;
+import services.SawMill;
+import services.CsvReader;
+import services.CsvWriter;
+
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        CsvReader csvReader = new CsvReader();
+        CsvWriter csvWriter = new CsvWriter();
+        SawMill sawMill = new SawMill();
+
+        List<Log> logs = csvReader.read("input.csv");
+        sawMill.process(logs);
 
 
     }
--- src/services/SawMill.java
+++ src/services/SawMill.java
@@ -1,4 +1,15 @@
 package services;
 
+import models.Log;
+import models.TreeType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SawMill {
+    public void process(List<Log> logs){
+        Map<TreeType, List<log>>
+    }
 }
