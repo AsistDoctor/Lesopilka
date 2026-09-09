2d7356985a95a6e6f5dcbb443eb60cbc729f5098
AsistDoctor <rebyssine@gmail.com>
2026-09-09 12:48:43 +0300

    отлов деревьев

--- src/services/CsvReader.java
+++ src/services/CsvReader.java
@@ -10,6 +10,9 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CsvReader {
 
     private int unknownCount = 0;
